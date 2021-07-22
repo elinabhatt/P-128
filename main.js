6e1b30d89db205c81333c66c404da692bc84408a
elinabhatt <74358269+elinabhatt@users.noreply.github.com>
2021-07-22 22:07:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,10 +30,14 @@ function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreRightWrist=" + scoreLeftWrist);
+
         leftWristX= results[0].pose.leftWrist.x;
         leftWristY= results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX +"leftWristY =" + leftWristY);
-
+ 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX =" + rightWristX +"rightWristY =" + rightWristY);
@@ -46,6 +50,52 @@ function modelLoaded() {
 
 function draw(){
     image(video,0,0,600,500)
+
+    fill("#FF0000");
+    stroke("FF0000")
+
+    if (scoreRightWrist > 0.2) 
+    {
+        circle(rightWristX,rightWristY,20)
+    if (rightWristY>0 && rightWristY <= 100) 
+  {
+    document.getElementById("speed").innerHTML = "Speed = 0.5x";
+    song.rate(0.5);
+  }
+    else if (rightWristY>100 && rightWristY <= 200) 
+  {
+    document.getElementById("speed").innerHTML = "Speed = 1x";
+    song.rate(1);
+  }
+    else if (rightWristY>200 && rightWristY <= 300) 
+  {
+    document.getElementById("speed").innerHTML = "Speed = 1.5x";
+    song.rate(1.5);
+  }
+    else if (rightWristY>300 && rightWristY <= 400) 
+  {
+    document.getElementById("speed").innerHTML = "Speed = 2x";
+    song.rate(2);
+  }
+    else if (rightWristY>400 && rightWristY <= 500) 
+  {
+    document.getElementById("speed").innerHTML = "Speed = 2x";
+    song.rate(2);
+  }
+    }          
+
+ if (scoreLeftWrist > 0.2) {
+
+    circle(leftWristX, leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimal/500;
+    document.getElementById("volume").innerHTML = "Volume =" + volume;
+    song.setVolume(volume);  
+ }
+      
+    
+    
 }
 
 
